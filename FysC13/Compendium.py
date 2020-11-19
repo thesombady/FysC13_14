4ad070ecdc35bdbcae2a9b72d12c4874a1b7d991
Andreas Evensen <andreas.evensen11@gmail.com>
2020-11-19 15:25:08 +0100

    tangental to x^2

--- FysC13/Compendium.py
+++ FysC13/Compendium.py
@@ -1,11 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+"""
+def GeneralPotential(r,a,b,n,m):
+    if n<m:
+        raise ValueError("Cant computer since n needs to be bigger than m")
+    else:
+        if isinstance(r, (np.ndarray, np.generic)):
+            return a/(r**n) - b/(r**m)
+potential = GeneralPotential(np.linspace(0.0001,3,100), 3, 18, 15, 9)
+plt.plot(np.linspace(0.0001,3,100), potential)
+plt.plot(0)
+plt.ylim(-50,50)
+plt.grid()
+plt.show()
+"""
 
-def P(a):
-    xlist = np.linspace(-5,-5,100)
-    ylist = np.exp(xlist*2*np.pi/a)
-    return xlist, ylist
-Values1 = P(10)
-plt.plot(Values1[0],Values1[1])
+def SheerStress(x):
+    return x**2
+
+def gradient(x):
+    div = 2*x
+    return np.linspace(0, 3, 100)*div - SheerStress(x)
+
+xlist = np.linspace(0, 3, 100)
+ylist = SheerStress(xlist)
+list1 = [0.5,1.8]
+plt.plot(xlist, ylist, label = r"Function $x^2$")
+for i in range(len(list1)):
+    plt.plot(xlist, gradient(list1[i]), '--', label = f"Tangent at {list1[i]}")
+plt.grid()
+plt.ylim(-1,5)
+plt.legend()
 plt.show()
