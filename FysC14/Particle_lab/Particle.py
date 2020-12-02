b986f65fa95f809a4802e4881a5f379b6950f35b
Andreas Evensen <andreas.evensen11@gmail.com>
2020-12-02 15:07:21 +0100

    ...

--- FysC14/Particle_lab/Particle.py
+++ FysC14/Particle_lab/Particle.py
@@ -2,9 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os, sys
+import io
 
 Path = os.path.join(os.getcwd(), "FysC14/Particle_lab/Data.txt")
-#Data = pd.read_txt()
+#Dataframe = pd.read_csv(Path)
+#print(Dataframe)
+#print(Dataframe.keys())
 
 with open(Path, "r") as file:
-    print(file.read())
+    Data = file.read()
+Dataset = io.StringIO(Data.decode('utf-8'))
+Dataframe = Dataset.readlines()
+print(Dataframe)
