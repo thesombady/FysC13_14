8cefbd77a17a5c20788eedfbb0eaac2edfa07e51
Andreas Evensen <andreas.evensen11@gmail.com>
2020-12-12 15:19:54 +0100

    Update Led.py

--- FysC13/Led.py
+++ FysC13/Led.py
@@ -9,15 +9,26 @@ Vol_ovr = np.array([2.682, 2.772, 2.842, 2.905, 2.956, 2.99, 3.039, 0.365, 0.866
 Current = np.array([0, 0.02, 0.03, 0.05, 0.07, 0.09, 0.10, 0, 0, 0, 0])
 Intnsty = np.array([0.03, 0.08, 0.13, 0.18, 0.21, 0.25, 0.29, 0, 0, 0, 0])
 wavelength = 452 * 10 **(-9) #452 nm
-
+"""
 def exp(a, b, x):
-	return a * np.exp(x*b)
+	return a*np.exp(-x/b)-a
 Expfit, Covarience = curve_fit(exp, Vol_ovr, Current)
+print(Expfit)
+"""
+"""
+Expfit, constants = pn.ExpRegression(Voltage, Current)
+xlist = np.linspace(0, 3, 100)
+ylist = Expfit(xlist)
+print(constants)
+""" #Works but is off.
 
-xlist = np.linspace(0,3.1, 100)
-ylist = exp(Expfit[0], Expfit[1], xlist)
+exp = lambda x,a,b: a * np.exp(-x/b)
+Expfit, Covarience = curve_fit(exp, Vol_ovr, Current)
+xlist = np.linspace(0,3, 100)
+ylist = exp(xlist, Expfit[0], Expfit[1])
 plt.plot(xlist, ylist, '--', label = "Fit")
 plt.plot(Vol_ovr, Current, '.', label = "Data")
 plt.grid()
 plt.legend()
+
 plt.show()
