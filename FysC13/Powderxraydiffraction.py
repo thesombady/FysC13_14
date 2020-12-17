d6fb59bfe9697060d16d788801403bd929249e21
Andreas Evensen <andreas.evensen11@gmail.com>
2020-12-17 16:32:30 +0100

    update

--- FysC13/Powderxraydiffraction.py
+++ FysC13/Powderxraydiffraction.py
@@ -9,16 +9,19 @@ PATH2 = os.path.join("/Users/andreasevensen/Desktop/XrayDiffraction", "Al2O3.xyd
 PATH3 = os.path.join("/Users/andreasevensen/Desktop/XrayDiffraction", "mixture.xyd")
 
 def Parser(Path):
-    with open(Path, 'r') as file:
-        Data = file.readlines()
-    Xlist = []
-    Ylist = []
-    for i in range(len(Data)):
-        Values = Data[i].split(' ')
-        val2 = Values[-1].replace("\n", '')
-        Xlist.append(float(Values[0]))
-        Ylist.append(float(val2))
-    return np.array(Xlist), np.array(Ylist)
+    try:
+        with open(Path, 'r') as file:
+            Data = file.readlines()
+        Xlist = []
+        Ylist = []
+        for i in range(len(Data)):
+            Values = Data[i].split(' ')
+            val2 = Values[-1].replace("\n", '')
+            Xlist.append(float(Values[0]))
+            Ylist.append(float(val2))
+        return np.array(Xlist), np.array(Ylist)
+    except:
+        raise Exception("[Parser]: Cant find the input")
 
 """
 AGData = Parser(PATH1)
